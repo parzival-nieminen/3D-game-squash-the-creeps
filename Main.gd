676ca5ac0613b273419227825601ad389cecffe2
Parzival <parzival.nieminen@gmail.com>
2021-08-08 23:48:20 +0200

    feat: add retry screen to restart the game
    * add ui retry screen
    * restart on Enter key if the game is stopped

--- Main.gd
+++ Main.gd
@@ -4,6 +4,10 @@ export (PackedScene) var mob_scene = preload("res://Mob.tscn")
 
 func _ready():
 	randomize()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
 
 func _on_MobTimer_timeout():
 	var mob = mob_scene.instance()
@@ -20,3 +24,4 @@ func _on_MobTimer_timeout():
 
 func _on_Player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
